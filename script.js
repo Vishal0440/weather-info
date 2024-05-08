c3ed9a77106716c202777a80dd6ee57551b471d5
Vishal0440 <prasadvishal70@gmail.com>
2024-05-08 13:34:23 +0530

    final commit

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function getWeather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
-  // console.log(data);
+  console.log(data);
   return showWeather(data);
 }
 
